b147a7b2d46b11386884b71cf38297dd7c636d3f
jana <janasameh92@gmail.com>
2025-09-09 23:29:54 +0300

    database created

--- hamada/Program.cs
+++ hamada/Program.cs
@@ -1,7 +1,12 @@
+using hamada.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
